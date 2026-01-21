e4e00d633bd6ac7cd1dd75b299d0eb90e98be90d
Siddhushady <siddhuduke3@gmail.com>
2026-01-21 11:16:49 +0530

    [Siddharth]: Rename oops packages

---
+++ src/objectorientedprogramming/constructors/EmployeeRecord.java
@@ -0,0 +1,25 @@
+package objectorientedprogramming.constructors.Level1;
+
+/*
+ * EmployeeRecord class
+ * --------------------
+ * Demonstrates protected access in subclasses.
+ */
+public class EmployeeRecord {
+
+    public int employeeID;
+    protected String department;
+    private double salary;
+
+    public void updateSalary(double salary) {
+        this.salary = salary;
+    }
+}
+
+// Subclass
+class ManagerRecord extends EmployeeRecord {
+
+    void displayManager() {
+        System.out.println(employeeID + " - " + department);
+    }
+}
